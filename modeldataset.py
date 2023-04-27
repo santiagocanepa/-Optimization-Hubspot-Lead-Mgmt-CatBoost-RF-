e4c84a30c5a3eb817a58fbbc86d5c95f211999a5
Santiago Canepa <125301263+canepasantiagoivan@users.noreply.github.com>
2023-04-27 17:23:16 -0300

    Update modeldataset.py

--- modeldataset.py
+++ modeldataset.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-# Leer el archivo de Excel
-db = 'C:/Users/Santiago/Py/dataset_FQC_1800.xlsx'
-df = pd.read_excel(db)
+archivo_excel = 'C:/Users/Electro-PC/Python/dataset_FQCTOTAL.xlsx'
+df = pd.read_excel(archivo_excel)
 
-# Filtra las filas
-df = df[(df['Nombre'].str.len() > 2) & (df['Nombre'] != 'Vista previa')]
+df = df[(df['Nombre'].str.len() > 2) & (df['Nombre'] != 'Vista previa')].head(500)
 
-df = df.head(500)
+# Reemplaza los términos no deseados en la columna 'Zona Horaria' directamente
+df['Zona Horaria'] = df['Zona Horaria'].str.replace('UTC -03:00', '').str.replace('UTC -04:00', '').str.replace('América', '').str.replace('Argentina', '').str.replace('(', '').str.replace(')', '')
 
-# Guarda el resultado en un nuevo archivo de Excel
-df.to_excel('dataset_FQC_1800.xlsx', index=False)
+# Guarda el DataFrame actualizado en un nuevo archivo Excel
+df.to_excel('dataset_FQC_1500_1800resultado.xlsx', index=False)
